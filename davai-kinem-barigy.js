df91ad3c9ae847308463a988f081d790f1485169
Rustam Mustafaev <re1gen9@mail.ru>
2025-04-13 23:06:50 +0300

    integrated chalk logs & updated boolean assign

--- davai-kinem-barigy.js
+++ davai-kinem-barigy.js
@@ -1,17 +1,23 @@
 import inquirer from 'inquirer'
-import {checkHeardPolkiloStatus, checkVebatDecisionStatus, checkMoneyExistenceStatus, checkSwearingAccessStatus} from "./properties-status.js"
+import chalk from 'chalk'
 import {resultSongLirycs} from "./song-lirycs-logs.js"
 import {MEETING_TIME_OPTION} from "./config.js"
 
-async function main() { 
-    const heardPolkiloMoment = await inquirer.prompt([
+async function main() {
+    await inquirer.prompt([
         {
           type: 'list',
           name: 'framework',
           message: 'Слышал про полкило?',
           choices: ['Да', 'Нет'],
         }
-      ])
+      ]).then(answers => {
+        if(answers.framework === 'Нет') {
+          console.log(chalk.yellow.bold('Вы не слышали про полкило, никакой ситуации не возникнет!'))
+          process.kill(process.pid)
+        }
+        return
+      })
 
     const vebatDecision = await inquirer.prompt([
         {
@@ -20,7 +26,12 @@ async function main() {
           message: 'Хочешь въебать барыгу?',
           choices: ['Да', 'Нет'],
         }
-      ])
+      ]).then(answers => {
+        if(answers.framework === 'Нет') {
+          return false
+        }
+        return true
+      })
 
     const moneyExistence = await inquirer.prompt([
         {
@@ -29,7 +40,12 @@ async function main() {
           message: 'У тебя много денег?',
           choices: ['Да', 'Нет'],
         }
-      ])
+      ]).then(answers => {
+        if(answers.framework === 'Нет') {
+          return false
+        }
+        return true
+      })
 
     const meetingTime = await inquirer.prompt([
         {
@@ -38,7 +54,9 @@ async function main() {
           message: 'На сколько назначишь встречу с барыгой?',
           choices: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
         }
-      ])
+      ]).then(answers => {
+        return Number(answers.framework)
+      })
 
     const swearingAccess = await inquirer.prompt([
         {
@@ -47,8 +65,14 @@ async function main() {
           message: 'Можешь общаться матом?',
           choices: ['Да', 'Нет'],
         }
-      ])
-    resultSongLirycs(checkHeardPolkiloStatus(heardPolkiloMoment), checkVebatDecisionStatus(vebatDecision), checkMoneyExistenceStatus(moneyExistence), meetingTime, checkSwearingAccessStatus(swearingAccess), MEETING_TIME_OPTION)
+      ]).then(answers => {
+        if(answers.framework === 'Нет') {
+          return false
+        }
+        return true
+      })
+    
+    resultSongLirycs(vebatDecision, moneyExistence, meetingTime, swearingAccess, MEETING_TIME_OPTION)
 }
 
 main()
--- song-lirycs-logs.js
+++ song-lirycs-logs.js
@@ -1,89 +1,92 @@
-export async function resultSongLirycs(heardPolKiloBoolean, vebatDecisionBoolean, moneyExistenceBoolean, meetingTimeInfo, swearingAccessBoolean, meetingTimeObject) {
-    console.log('\n< -------- ТЕКСТ ПЕСНИ -------- >\n')
-    if(!heardPolKiloBoolean) {
-        console.log('Я не слышал что у барыги есть на хате целых полкило!\n')
-        return
-    } else {
-        console.log('Я слышал у барыги есть на хате целых полкило...')
-    }
+import chalk from 'chalk'
+
+const songLog = (text) => {
+    return console.log(chalk.white.bold(text))
+}
+
+export async function resultSongLirycs(vebatDecisionBoolean, moneyExistenceBoolean, meetingTimeInfo, swearingAccessBoolean, meetingTimeObject) {
+    console.log(chalk.blue.bold('\n< -------- ТЕКСТ ПЕСНИ -------- >\n'))
+
+    songLog('Я слышал у барыги есть на хате целых полкило...')
     if(vebatDecisionBoolean && swearingAccessBoolean) {
-        console.log('Ну так чего мы ждём? Давай поедем и въебём его!')
+        songLog('Ну так чего мы ждём? Давай поедем и въебём его!')
     } else if (vebatDecisionBoolean){
-        console.log('Ну так чего мы ждём? Давай поедем и кинем его!')
+        songLog('Ну так чего мы ждём? Давай поедем и кинем его!')
     }
-        console.log('Целых полкило?! - Да, целых полкило!')
+        songLog('Целых полкило?! - Да, целых полкило!')
     if(vebatDecisionBoolean && swearingAccessBoolean) {
-        console.log('Ну так чего мы ждём? Давай поедем и въебём его!')
+        songLog('Ну так чего мы ждём? Давай поедем и въебём его!')
     } else if (vebatDecisionBoolean) {
-        console.log('Ну так чего мы ждём? Давай поедем и кинем его!')
+        songLog('Ну так чего мы ждём? Давай поедем и кинем его!')
     }
     if(vebatDecisionBoolean) {
-        console.log(`Давай кинем барыгу, давай кинем барыгу!\nДавай кинем барыгу, давай кинем барыгу!\nДавай кинем барыгу, давай кинем барыгу!\nДавай кинем барыгу, давай кинем барыгу!`)
+        songLog(`Давай кинем барыгу, давай кинем барыгу!\nДавай кинем барыгу, давай кинем барыгу!\nДавай кинем барыгу, давай кинем барыгу!\nДавай кинем барыгу, давай кинем барыгу!`)
     }
     if(!moneyExistenceBoolean) {
-        console.log('Trappa позвонил и мне сказал: Sil, я хочу на море(\nНо на кармане ни рубля — все деньги в обороте!')
+        songLog('Trappa позвонил и мне сказал: Sil, я хочу на море(\nНо на кармане ни рубля — все деньги в обороте!')
     }
-        console.log(`А на районе есть барыга, и без крыши, вроде...\nЕму сегодня подвезут полкилчика на block'e)`)
+        songLog(`А на районе есть барыга, и без крыши, вроде...\nЕму сегодня подвезут полкилчика на block'e)`)
     if(vebatDecisionBoolean) {
-        console.log(`Давай, давай кинем барыгу...\nЯ не борец, но с прогибом давай кинем барыгу!\nПолкило — это вес, давай кинем барыгу`)
+        songLog(`Давай, давай кинем барыгу...\nЯ не борец, но с прогибом давай кинем барыгу!\nПолкило — это вес, давай кинем барыгу`)
     }
     if(vebatDecisionBoolean && !moneyExistenceBoolean) {
-        console.log('И потом все на рейс отдыхать за барыгу))')
+        songLog('И потом все на рейс отдыхать за барыгу))')
     }
     if(vebatDecisionBoolean) {
-        console.log('Алло, Sil, у меня есть один сладенький кусок...')
+        songLog('Алло, Sil, у меня есть один сладенький кусок...')
     }
     if(vebatDecisionBoolean && meetingTimeInfo) {
-        console.log(`Мы забились с ним на ${meetingTimeObject[Number(meetingTimeInfo['framework'])]}: он на хате меня ждёт`)
+        songLog(`Мы забились с ним на ${meetingTimeObject[meetingTimeInfo]}: он на хате меня ждёт`)
     }
     if(!moneyExistenceBoolean) {
-        console.log(`Я хочу его Porsche Cayenne и часы Hublot!\nЯ хочу, чтобы он лежал НА ПОЛУ ЛИЦОМ!`)
+        songLog(`Я хочу его Porsche Cayenne и часы Hublot!\nЯ хочу, чтобы он лежал НА ПОЛУ ЛИЦОМ!`)
     }
     if(vebatDecisionBoolean) {
-        console.log('Не вопрос: мы берём стволы, и мы едем на район\nЯ очень надеюсь, что дерьмо будет с собой при нём!')
+        songLog('Не вопрос: мы берём стволы, и мы едем на район\nЯ очень надеюсь, что дерьмо будет с собой при нём!')
     }
     if(vebatDecisionBoolean && swearingAccessBoolean) {
-        console.log('Это выстрел, это факт — въебать его и нам съебать')
+        songLog('Это выстрел, это факт — въебать его и нам съебать')
     }
     if(!moneyExistenceBoolean) {
-        console.log('Ненавижу зиму в Раше: нам на пляж — и отдыхать!')
+        songLog('Ненавижу зиму в Раше: нам на пляж — и отдыхать!')
     }
     if(vebatDecisionBoolean && swearingAccessBoolean) {
-        console.log('Он слышал то, что я мутил, давай его ВЪЕБЁМ!')
+        songLog('Он слышал то, что я мутил, давай его ВЪЕБЁМ!')
     }
-    console.log('Он говорит, у него полкило чистого коко...')
+    songLog('Он говорит, у него полкило чистого коко...')
     if(swearingAccessBoolean) {
-        console.log('В пизду любых барыг, кроме моих!\nМы любим делать грязь, чтобы в ней вырастить цветы!')
+        songLog('В пизду любых барыг, кроме моих!\nМы любим делать грязь, чтобы в ней вырастить цветы!')
     }
     if(vebatDecisionBoolean && swearingAccessBoolean) {
-        console.log('Мы берём его, но, бля, по факту мы берём своё!\nНехуй продавать там, где живёшь, и портить мой район')
+        songLog('Мы берём его, но, бля, по факту мы берём своё!\nНехуй продавать там, где живёшь, и портить мой район')
     } else if(vebatDecisionBoolean) {
-        console.log('Мы берём его, но, блин, по факту мы берём своё!\nНефиг продавать там, где живёшь, и портить мой район')
+        songLog('Мы берём его, но, блин, по факту мы берём своё!\nНефиг продавать там, где живёшь, и портить мой район')
     }
-    console.log('Бабушки на падике кричат: "Антоха продаёт!"')
-    if(swearingAccessBoolean) {
-        console.log('Теперь Антоха копит, чтоб закрыть с поставщиком проёб!\nНе сри там, где живёшь, или там, где живу я\nИли там, где живёт Sil-A\nДолбоёб, тебе пизда!')
+    songLog('Бабушки на падике кричат: "Антоха продаёт!"')
+    if(vebatDecisionBoolean && swearingAccessBoolean) {
+        songLog('Теперь Антоха копит, чтоб закрыть с поставщиком проёб!')
     }
+    songLog('Не сри там, где живёшь, или там, где живу я\nИли там, где живёт Sil-A')
     if(!swearingAccessBoolean) {
-        console.log('Я не могу общаться матом((')
-    } else console.log('Я могу общаться матом — НАХУЙ этого типа')
+        songLog('Я не могу общаться матом((')
+    } else songLog('Долбоёб, тебе пизда!\nЯ могу общаться матом — НАХУЙ этого типа')
     if(vebatDecisionBoolean) {
-        console.log('Мы под кайфом всё отжали, мы на новых номерах')
+        songLog('Мы под кайфом всё отжали, мы на новых номерах')
     }
-        console.log('Я слышал у барыги есть на хате целых полкило...')
+    songLog('Я слышал у барыги есть на хате целых полкило...')
     if(vebatDecisionBoolean && swearingAccessBoolean) {
-        console.log('Ну так чего мы ждём? Давай поедем и въебём его!')
+        songLog('Ну так чего мы ждём? Давай поедем и въебём его!')
     } else if (vebatDecisionBoolean) {
-        console.log('Ну так чего мы ждём? Давай поедем и кинем его!')
+        songLog('Ну так чего мы ждём? Давай поедем и кинем его!')
     }
-        console.log('Целых полкило?! - Да, целых полкило!')
+    songLog('Целых полкило?! - Да, целых полкило!')
     if(vebatDecisionBoolean && swearingAccessBoolean) {
-        console.log('Ну так чего мы ждём? Давай поедем и въебём его!')
+        songLog('Ну так чего мы ждём? Давай поедем и въебём его!')
     } else if (vebatDecisionBoolean) {
-        console.log('Ну так чего мы ждём? Давай поедем и кинем его!')
+        songLog('Ну так чего мы ждём? Давай поедем и кинем его!')
     }
     if(vebatDecisionBoolean) {
-        console.log(`Давай кинем барыгу, давай кинем барыгу!\nДавай кинем барыгу, давай кинем барыгу!\nДавай кинем барыгу, давай кинем барыгу!\nДавай кинем барыгу, давай кинем барыгу!`)
+        songLog(`Давай кинем барыгу, давай кинем барыгу!\nДавай кинем барыгу, давай кинем барыгу!\nДавай кинем барыгу, давай кинем барыгу!\nДавай кинем барыгу, давай кинем барыгу!`)
     }
-    console.log('')
+    songLog('')
 }
